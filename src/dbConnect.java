db9c71da465e979e624977b1b89dafc18a29ad10
Tianqi Jin <jackjinn77817@gmail.com>
2015-11-22 00:03:20 -0700

    create the main class and dbConnect class

---
+++ src/dbConnect.java
@@ -0,0 +1,5 @@
+/**
+ * Created by tjin on 2015-11-22.
+ */
+public class dbConnect {
+}
---
+++ src/saleSystem.java
@@ -0,0 +1,8 @@
+/**
+ * Created by tjin on 2015-11-21.
+ */
+public class saleSystem {
+    public static void main(String []args){
+
+    }
+}
