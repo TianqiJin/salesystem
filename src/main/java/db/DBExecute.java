5df66925ccb6a877a6e78ca47b1aca77e4322c38
Tianqi Jin <jackjinn77817@gmail.com>
2015-11-22 22:39:28 -0700

    Abstact DBExecute. Create DBExecuteProduct inheriting from DBExecute. Refactor DBQueries and create ProductModel

--- src/main/java/db/DBExecute.java
+++ src/main/java/db/DBExecute.java
@@ -1,27 +1,16 @@
 package db;
 
-import org.apache.log4j.Logger;
-
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
-import static db.DBQueries.Queries.*;
+
 /**
  * Created by jiawei.liu on 11/22/15.
  */
-public class DBExecute {
-    private static Logger logger= Logger.getLogger(DBExecute.class);
+public abstract class DBExecute {
 
-    public static String selectFromProduct(int id){
-        try {
-            List<Map<String, Object>> result = dbConnect.executeQuery(SELECT_TEST, ObjectDeserializer.DEFAULT_DESERIALIZER,id);
-            if (result != null) {
-                return result.get(0).get("Texture").toString();
-            }
-        } catch (SQLException e) {
-            logger.error(e.getMessage());
-        }
-        return "Nothing";
-    }
+    protected static List<Map<String, Object>> selectResult;
 
+    public abstract String selectFromDatabase(int id);
+    public abstract String insertIntoDatabase();
+    public abstract String updateDatabase();
 }
